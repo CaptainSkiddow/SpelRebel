8286c5f55c5c6b6ab1039bdbe47ee482dd90008a
EgbertLudema <EgbertLudema2001@gmail.com>
2023-12-15 12:31:00 +0100

    1st Updates after consult Jop

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Function to calculate dynamic height based on scroll position
-    function calculateHeight(initialHeight, scrollPos, maxScroll, maxHeight) {
-        if (scrollPos >= maxScroll) return maxHeight;
-        const heightRange = maxHeight - initialHeight;
-        const heightProgress = scrollPos / maxScroll;
-        return initialHeight + (heightRange * heightProgress);
-    }
-    
     // Function to calculate translation based on scroll position
     function calculateTranslation(initialTranslateY, scrollPos, rate) {
         return initialTranslateY + scrollPos * rate;
@@ -21,24 +13,15 @@ document.addEventListener('DOMContentLoaded', () => {
     // Scroll event listener
     window.addEventListener('scroll', () => {
         const scrollPos = window.scrollY;
-        const windowHeight = window.innerHeight;
-
-        // Custom settings for the sunset background
-        const initialHeight = 380; // Example initial height, adjust as needed
-        const maxHeight = windowHeight; // Full viewport height
-
-        // Calculate and set the height of the sunset background
-        const backgroundHeight = calculateHeight(initialHeight, scrollPos, 10, maxHeight);
-        document.getElementById('img1').style.height = `${backgroundHeight}px`;
 
         // Custom settings for each image (initial scale, scale rate, max scale, initial translateY, translateY rate)
         const imageSettings = [
             // Sunset
             { initialScale: 1, scaleRate: 0, maxScale: 1, initialTranslateY: 0, translateYRate: 0 },
             // Sun
-            { initialScale: 0.5, scaleRate: 0.05, maxScale: 5, initialTranslateY: -10, translateYRate: 0.5 },
+            { initialScale: 0.5, scaleRate: 0.05, maxScale: 5, initialTranslateY: 0, translateYRate: 0.5 },
             // Water/reflection
-            { initialScale: 1, scaleRate: 0, maxScale: 1, initialTranslateY: -100, translateYRate: 12 },
+            { initialScale: 1, scaleRate: 0, maxScale: 1, initialTranslateY: 0, translateYRate: 0 },
             // Yoga
             { initialScale: 0.4, scaleRate: 0.07, maxScale: 6, initialTranslateY: 0, translateYRate: 0.5 },
             // Beach
