b4d172a3c478255eb22279d00b86186bd9282663
EgbertLudema <EgbertLudema2001@gmail.com>
2023-12-18 13:58:18 +0100

    Updates
    Updated some images, added containers and updated the scroll effect to be (a little) more responsive.

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ document.addEventListener('DOMContentLoaded', () => {
         return Math.min(scale, maxScale);
     }
 
+    // Function to convert vh to pixels
+    function vhToPixels(vh) {
+        return vh * document.documentElement.clientHeight / 100;
+    }
+
+    // Function to calculate height based on scroll position
+    function calculateHeight(initialHeightVh, scrollPos, rate, maxHeightVh) {
+        const initialHeightPx = vhToPixels(initialHeightVh);
+        const maxHeightPx = vhToPixels(maxHeightVh);
+        const height = initialHeightPx + scrollPos * rate;
+        return Math.min(height, maxHeightPx);
+    }
+
     // Scroll event listener
     window.addEventListener('scroll', () => {
         const scrollPos = window.scrollY;
@@ -17,22 +30,42 @@ document.addEventListener('DOMContentLoaded', () => {
         // Custom settings for each image (initial scale, scale rate, max scale, initial translateY, translateY rate)
         const imageSettings = [
             // Sunset
-            { initialScale: 1, scaleRate: 0, maxScale: 1, initialTranslateY: 0, translateYRate: 0 },
+            { initialScale: 1, scaleRate: 0, maxScale: 1, initialTranslateY: 0, translateYRate: 0, customTransform: '' },
             // Sun
-            { initialScale: 0.5, scaleRate: 0.05, maxScale: 5, initialTranslateY: 0, translateYRate: 0.5 },
+            { initialScale: 0.5, scaleRate: 0.2, maxScale: 12, initialTranslateY: 0, translateYRate: 0.5, customTransform: '' },
             // Water/reflection
-            { initialScale: 1, scaleRate: 0, maxScale: 1, initialTranslateY: 0, translateYRate: 0 },
-            // Yoga
-            { initialScale: 0.4, scaleRate: 0.07, maxScale: 6, initialTranslateY: 0, translateYRate: 0.5 },
-            // Beach
-            { initialScale: 1, scaleRate: 0, maxScale: 1, initialTranslateY: 80, translateYRate: 24 }
+            { initialScale: 1, scaleRate: 0, maxScale: 1, initialTranslateY: 0, translateYRate: 10, customTransform: '' },
+            // Yoga+Beach
+            { initialScale: 1, scaleRate: 0.5, maxScale: 35, initialTranslateY: 0, translateYRate: 0, customTransform: 'translate(0%, 27%)' },
         ];
 
-        // Apply scale and translation to each image
+        // Additional settings for img1 height change
+        const img1HeightSettings = {
+            initialHeight: 54,
+            heightRate: 10,
+            maxHeight: 100
+        };
+
+        // Apply scale, translation, custom transform, and height change to each image
         imageSettings.forEach((settings, index) => {
             const imgScale = calculateScale(settings.initialScale, scrollPos, settings.scaleRate, settings.maxScale);
             const imgTranslate = calculateTranslation(settings.initialTranslateY, scrollPos, settings.translateYRate);
-            document.getElementById(`img${index + 1}`).style.transform = `scale(${imgScale}) translateY(${imgTranslate}px)`;
+
+            const img = document.getElementById(`img${index + 1}`);
+
+            // Combine new transformations with custom ones
+            let transformStyles = `scale(${imgScale})`;
+            if (settings.translateYRate !== 0) {
+                transformStyles += ` translateY(${imgTranslate}px)`;
+            }
+            transformStyles += ' ' + settings.customTransform;
+            img.style.transform = transformStyles;
+
+            // Apply height change to img1
+            if (index === 0) {
+                const imgHeight = calculateHeight(img1HeightSettings.initialHeight, scrollPos, img1HeightSettings.heightRate, img1HeightSettings.maxHeight);
+                img.style.height = `${imgHeight}px`;
+            }
         });
     });
 });
