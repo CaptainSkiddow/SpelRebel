e2adc5d558bef03692d579a6ae0311aee2a53abf
CaptainSkiddow <dekkerrens@outlook.com>
2023-12-18 14:41:28 +0100

    x
    x

--- index.js
+++ index.js
@@ -15,5 +15,34 @@ gsap.to(sections, {
     }
 });
 
+$(function () {
+    $(".outer").mousemove(function (e) {
+        var outer = $(this);
+        var cursor = outer.find(".cursor");
+        var inner = outer.find(".inner");
+
+        var left = e.pageX - outer.offset().left - cursor.outerWidth() / 2;
+        var top = e.pageY - outer.offset().top - cursor.outerHeight() / 2;
+
+        if (left < 0) {
+            left = 0;
+        }
+        if (top < 0) {
+            top = 0;
+        }
+        if (left + cursor.outerWidth() > outer.outerWidth()) {
+            left = outer.outerWidth() - cursor.outerWidth();
+        }
+        if (top + cursor.outerHeight() > outer.outerHeight()) {
+            top = outer.outerHeight() - cursor.outerHeight();
+        }
+
+        cursor.show().css({ left: left, top: top });
+        inner.css({ left: -left, top: -top });
+    }).mouseleave(function () {
+        $(this).find(".cursor").hide();
+    });
+});
+
 
 
